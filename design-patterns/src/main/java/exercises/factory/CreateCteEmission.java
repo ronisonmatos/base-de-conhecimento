610d847a884fee9cc18de4fda260159783596ab0
Ronison <ronison.matos@neogrid.com>
2022-03-18 12:00:23 -0300

    Exercíco de Factory Method

--- design-patterns/src/main/java/exercises/factory/CreateCteEmission.java
+++ design-patterns/src/main/java/exercises/factory/CreateCteEmission.java
@@ -4,6 +4,11 @@ import exercises.factory.emission.Cte;
 import exercises.factory.emission.IEmission;
 
 public class CreateCteEmission extends Document{
+
+    /* 5 - Os Criadores de Concreto substituem o método de fábrica base para que ele retorne um tipo diferente de produto.
+     * Observe que o método de fábrica não precisa criar novas instâncias o tempo tod0. Ele também pode retornar objetos existentes de um cache,
+     * um pool de objetos ou outra fonte.
+     */
     @Override
     protected IEmission createEmission() {
         return new Cte();
--- design-patterns/src/main/java/exercises/factory/CreateMdfeEmission.java
+++ design-patterns/src/main/java/exercises/factory/CreateMdfeEmission.java
@@ -5,6 +5,11 @@ import exercises.factory.emission.IEmission;
 import exercises.factory.emission.Mdfe;
 
 public class CreateMdfeEmission extends Document{
+
+    /* 5 - Os Criadores de Concreto substituem o método de fábrica base para que ele retorne um tipo diferente de produto.
+     * Observe que o método de fábrica não precisa criar novas instâncias o tempo tod0. Ele também pode retornar objetos existentes de um cache,
+     * um pool de objetos ou outra fonte.
+     */
     @Override
     protected IEmission createEmission() {
         return new Mdfe();
